05e1257d7eae1f130ed058778b192a67d4675c1d
dev-Gois <mattheusgoiis@gmail.com>
2023-12-18 18:52:11 -0300

    feat: create login/register and router

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,17 @@
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
+import Login from './views/Login'
+import Register from './views/Register'
+
 function App() {
 
   return (
-    <p>eae</p>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Navigate to="/login" />} />
+        <Route path="/login" element={<Login />} />
+        <Route path='/register' element={<Register />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
---
+++ src/theme/MainTheme.jsx
@@ -0,0 +1,11 @@
+import { createTheme } from "@mui/material";
+import { teal, grey } from "@mui/material/colors";
+
+const MainTheme = createTheme({
+  palette: {
+    primary: teal,
+    secondary: grey,
+  }
+})
+
+export default MainTheme
